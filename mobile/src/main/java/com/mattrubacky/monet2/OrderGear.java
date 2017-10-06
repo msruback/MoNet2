5d9c5bb3ab69fd9192421ee422f50d19ca4e5d34
Matthew Rubacky <mattrubacky@gmail.com>
2017-10-06 18:52:32 -0400

    Added support for ordering through notifications

--- mobile/src/main/java/com/mattrubacky/monet2/OrderGear.java
+++ mobile/src/main/java/com/mattrubacky/monet2/OrderGear.java
@@ -1,6 +1,7 @@
 package com.mattrubacky.monet2;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
@@ -9,11 +10,25 @@ import android.os.IBinder;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 
+import com.google.gson.Gson;
+
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 /**
  * Created by mattr on 10/5/2017.
  */
 
 public class OrderGear extends Service {
+
+    Product buying;
 
     @Nullable
     @Override
@@ -26,8 +41,37 @@ public class OrderGear extends Service {
 
         super.onStartCommand(intent,flags,startId);
         SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-
+        buying = intent.getExtras().getParcelable("product");
+        Thread thread = new Thread(orderGear);
+        thread.start();
 
         return START_NOT_STICKY;
     }
+    private Runnable orderGear = new Runnable() {
+        public void run() {
+            try{
+                SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                String cookie = settings.getString("cookie","");
+                String id = settings.getString("unique_id","");
+                Retrofit retrofit = new Retrofit.Builder().baseUrl("https://app.splatoon2.nintendo.net").addConverterFactory(GsonConverterFactory.create()).build();
+                Splatnet splatnet = retrofit.create(Splatnet.class);
+                RequestBody override = RequestBody.create(MediaType.parse("text/plain"), "1");
+                Call<ResponseBody> buy = splatnet.orderMerch(buying.id,id,override,cookie);
+                okhttp3.Request request = buy.request();
+                Response response = buy.execute();
+                Gson gson = new Gson();
+                Annie shop = gson.fromJson(settings.getString("shopState",""),Annie.class);
+                shop.ordered.gear = buying.gear;
+                shop.ordered.price = buying.price;
+                shop.ordered.skill = buying.skill;
+
+                SharedPreferences.Editor edit = settings.edit();
+
+                edit.putString("shopState",gson.toJson(shop));
+                edit.commit();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    };
 }
